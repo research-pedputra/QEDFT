44c6d25c9c1751ce2fd23596cc1800244790d74f
P E D Putra <50206087+research-pedputra@users.noreply.github.com>
2024-08-20 08:59:11 +0800

    Update background.py

--- XRD/background.py
+++ XRD/background.py
@@ -1,40 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
 
 class BackSub:
     def __init__(self, x, y):
-        """
-        Initialize the BackSub object with x and y data.
-
-        Parameters
-        ----------
-        x : array-like
-            The x-axis data.
-        y : array-like
-            The y-axis data.
-        """
         self.x = np.array(x)
-        self.y = np.array(y)
+        self.y = savgol_filter(np.array(y), 11, 3)
     
     def backsub(self, tol=1, show=False):
-        """
-        Perform background subtraction operation.
-
-        Parameters
-        ----------
-        tol : float, optional
-            Tolerance for background subtraction.
-            Default value is 1.
-        show : bool, optional
-            Whether to show the plot of the XRD chart.
-            Default value is False.
-        
-        Returns
-        -------
-        tuple
-            A tuple containing the x data and the background-subtracted y data.
-        """
-        
         L = len(self.y)
         lmda = int(0.50 * L / (self.x[0] - self.x[L-1]))  # Approx. # points for half width of peaks
 
@@ -48,11 +21,6 @@ class BackSub:
         
         # Update y data with background-subtracted values
         self.y = backsub_y
-
-        if show:
-            plt.plot(self.x, self.y)
-            plt.show()
-
         return self.x, backsub_y
 
 # Example usage:
